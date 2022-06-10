adf6e5f8c666c985b9872d093137a05397eef0a4
siszero <64197543+siszero@users.noreply.github.com>
2022-06-10 17:01:19 +0900

    Add files via upload

---
+++ 프로세스 스케줄링 시뮬레이터/Scheduling_Simulator/src/com/func/RR.java
@@ -0,0 +1,39 @@
+package com.func;
+import com.pro.Process;
+public class RR extends Scheduling {
+	int quantum;
+	public RR(int processorNum, int quantum) {
+		super(processorNum);
+		this.quantum = quantum;
+	}
+	@Override
+	public Process[] doScheduling() {
+		int index=0;
+		Process[] rePro=new Process[15];
+		printProcessInfo();
+		for (int time = 0; !isEnd(); time++) {
+			setWaitQ(time);
+			for (int i = 0; i < processorNum; i++) {
+				if (processor[i] != null) {
+					if(checkRemain(time, i)) {
+						rePro[index++]=processor[i];
+						processor[i] = null;
+					}else if (processor[i].getQuantum() == quantum - 1) {
+						processor[i].setQuantum(0);
+						processor[i].reduRemainTime();
+						waitQ.offer(processor[i]);
+						processor[i] = null;
+					}
+				}
+				if (processor[i] == null) {
+					if (!waitQ.isEmpty())
+						processor[i] = waitQ.pop();
+				} else {
+					processor[i].reduRemainTime();
+					processor[i].setQuantum(processor[i].getQuantum() + 1);
+				}
+			}
+		}
+		return rePro;
+	}
+}
---
+++ 프로세스 스케줄링 시뮬레이터/Scheduling_Simulator/src/com/pro/Process.java
@@ -0,0 +1,104 @@
+package com.pro;
+
+public class Process {
+	int arrTime;	//arrival time
+	int burTime;	//burst time
+	int endTime;	//end time
+	int ID;			//process ID
+	int waitTime;	//waiting time
+	int turnTime;	//turnaround time
+	int remainTime;	//remain time
+	int quantum;	//quantum (Round Robin)
+	public Process() {}
+	public Process(Process p) {
+		this.arrTime = p.arrTime;
+		this.burTime = p.burTime;
+		this.endTime = p.endTime;
+		this.waitTime = p.waitTime;
+		this.turnTime = p.turnTime;
+		this.remainTime = p.remainTime;
+		this.ID = p.ID;
+	}
+	public Process(int arrTime, int burTime) {
+		this.arrTime=arrTime;
+		this.burTime=burTime;
+		this.remainTime=burTime;
+		waitTime=0;  
+		turnTime=0;
+		quantum=0;
+	}
+	// 테스트용
+	public Process(int arrTime, int burTime, int endTime, int waitTime, int turnTime) {
+		super();
+		this.arrTime = arrTime;
+		this.burTime = burTime;
+		this.endTime = endTime;
+		this.waitTime = waitTime;
+		this.turnTime = turnTime;
+	}
+	public void addWaitTime() {
+		waitTime++;
+	}
+	public void reduRemainTime() {
+		remainTime--;
+	}
+	public int getRemainTime() {
+		return remainTime;
+	}
+	public void setRemainTime(int remainTime) {
+		this.remainTime = remainTime;
+	}
+	public int getArrTime() {
+		return arrTime;
+	}
+	public void setArrTime(int arrTime) {
+		this.arrTime = arrTime;
+	}
+	public int getBurTime() {
+		return burTime;
+	}
+	public void setBurTime(int burTime) {
+		this.burTime = burTime;
+	}
+	public int getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getWaitTime() {
+		return waitTime;
+	}
+	public void setWaitTime(int waitTime) {
+		this.waitTime = waitTime;
+	}
+	public int getTurnTime() {
+		return turnTime;
+	}
+	public void setTurnTime(int turnTime) {
+		this.turnTime = turnTime;
+	}
+	public int getQuantum() {
+		return quantum;
+	}
+	public void setQuantum(int quantum) {
+		this.quantum = quantum;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		return "Process [arrTime=" + arrTime + ", burTime=" + burTime + ", endTime=" + endTime + ", waitTime="
+				+ waitTime + ", turnTime=" + turnTime +", remainTime="+remainTime + "]";
+	}
+}
